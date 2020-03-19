456e59c92dc6b75f43351d2d7a0fc3db68b775b2
Manuel Salazar <jmcazarez@gmail.com>
2020-03-18 23:10:16 -0700

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-row>
+          <v-col sm="12" align="center" justify="center" cols="12">
+            <v-avatar size="80">
+              <img
+                src="~/assets/nuxt-icon.png"
+                alt="Nuxt Icon"
+              >
+            </v-avatar>
+            <h1 class="title">
+              NuxtJS and Vuetify
+            </h1>
+            <p class="caption">Starterkit</p>
+          </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
